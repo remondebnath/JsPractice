3e9a1b0c4de8fdee0ecec9d3d6ab4f80868ac154
Remon <remondebnath88@gmail.com>
2023-07-27 02:33:13 +0600

    commit

--- string.js
+++ string.js
@@ -5,3 +5,10 @@ console.log(text.split(""));
 console.log(text.split(" "))
 console.log(text.split(","))
 
+
+
+// JavaScript String match()
+let text = "The rain in SPAIN stays mainly in the plain";
+
+console.log(text.match("ain"))
+console.log(text.match(/ain/gi))
