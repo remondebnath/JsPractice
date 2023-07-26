c7674827bd2dea04d00a7e0f4cf65da2c856feea
Remon <remondebnath88@gmail.com>
2023-07-27 02:33:02 +0600

    commit

--- main.js
+++ main.js
@@ -1,19 +1,2 @@
-function main(input) {
-  const numbers = input.trim().split(' ');
-  const number1 = parseFloat(numbers[0]);
-  const number2 = parseFloat(numbers[1]);
 
- 
-    const sum = number1 + number2;
-    console.log(sum);
-  
-  // const name= input.trim();
-  
-  // console.log(`${name},good morning good morning`)
-
-}
-
-process.stdin.on('data', data => {
-  main(data.toString());
-});
 
---
+++ string.js
@@ -0,0 +1,7 @@
+// string split
+let text = "HELLO WORLD";
+
+console.log(text.split(""));
+console.log(text.split(" "))
+console.log(text.split(","))
+
