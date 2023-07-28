b5ebff0c7139f19fc4d9b957eb5650605b6bf801
Remon <remondebnath88@gmail.com>
2023-07-28 15:16:56 +0600

    commit

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-process.stdin.on('data', data => {
-    main(data.toString().trim());
-})
+// process.stdin.on('data', data => {
+//     main(data.toString().trim());
+// })
+// const result=[]
+// function rangeOfNumbers(startNum, endNum) {
+//     for(i=startNum;i<=endNum;i++)
+//       result.push(i)
+//      return result
+//   };
+
+//   console.log(rangeOfNumbers(5,10));
+  
+document.write
